64f1902ff92d18eb5f8fc6e10a4580a4d0f34631
taehoon <xogns1514@gmail.com>
2023-08-22 20:22:44 +0900

    feat: chap3. 람다를 이용한 filter와 sort

--- src/main/java/org/chap03/Lambdas.java
+++ src/main/java/org/chap03/Lambdas.java
@@ -1,12 +1,28 @@
 package org.chap03;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Lambdas {
     public static void main(String[] args) {
         Runnable r = () -> System.out.println("Hello World");
         r.run();
+
+        List<Apple> inventory = Arrays.asList(
+                new Apple(80, Color.GREEN),
+                new Apple(100, Color.RED),
+                new Apple(120, Color.RED)
+        );
+        //[Apple{color=GREEN, weight=80}]
+        List<Apple> greenApples = filter(inventory, a -> a.getColor() == Color.GREEN);
+        System.out.println(greenApples);
+
+        Comparator<Apple> c = (a1, a2) -> a1.getWeight() - a2.getWeight();
+
+        inventory.sort(c);
+        System.out.println(inventory);
 
     }
 
