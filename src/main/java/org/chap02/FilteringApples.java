6169bd77ad95a0af547cd5ad203f17c2330480b1
taehoon <xogns1514@gmail.com>
2023-08-22 19:33:08 +0900

    feat: chap2. for문 내에서 GREEN 필터링

--- src/main/java/org/chap02/FilteringApples.java
+++ src/main/java/org/chap02/FilteringApples.java
@@ -1,7 +1,38 @@
 package org.chap02;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.chap02.FilteringApples.Color.GREEN;
+
 public class FilteringApples {
     public static void main(String[] args) {
+        List<Apple> inventory = Arrays.asList(
+                new Apple(80, GREEN),
+                new Apple(90, GREEN),
+                new Apple(100, Color.RED),
+                new Apple(120, Color.RED),
+                new Apple(150, Color.RED)
+        );
+
+        //1. for문 내에서 GREEN 필터링
+        //[Apple{weight=80, color='GREEN'}, Apple{weight=90, color='GREEN'}]
+        List<Apple> greenApples = filterGreenApples(inventory);
+        System.out.println(greenApples);
+
+    }
+
+    //List를 받아 초록 사과 필터링
+    // for문 내부에서 enum GREEN과 일치 여부를 따져서 필터링.
+    //다른 색으로 필터링 하기위해서는 같은 형식의 함수 생성이 필요.
+    public static List<Apple> filterGreenApples(List<Apple> apples) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : apples) {
+            if (apple.getColor() == GREEN)
+                result.add(apple);
+        }
+        return result;
     }
 
     enum Color {
@@ -11,10 +42,9 @@ public class FilteringApples {
 
     public static class Apple {
         private int weight;
+        private Color color;
 
-        private String color = "";
-
-        public Apple(int weight, String color) {
+        public Apple(int weight, Color color) {
             this.weight = weight;
             this.color = color;
         }
@@ -27,11 +57,11 @@ public class FilteringApples {
             this.weight = weight;
         }
 
-        public String getColor() {
+        public Color getColor() {
             return color;
         }
 
-        public void setColor(String color) {
+        public void setColor(Color color) {
             this.color = color;
         }
 
