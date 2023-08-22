15df5d58f1e851d98739ed5ae19b130c01ac5875
taehoon <xogns1514@gmail.com>
2023-08-22 19:25:05 +0900

    feat: chap2. Color enum, Apple class 생성

---
+++ src/main/java/org/chap02/FilteringApples.java
@@ -0,0 +1,45 @@
+package org.chap02;
+
+public class FilteringApples {
+    public static void main(String[] args) {
+    }
+
+    enum Color {
+        RED,
+        GREEN
+    }
+
+    public static class Apple {
+        private int weight;
+
+        private String color = "";
+
+        public Apple(int weight, String color) {
+            this.weight = weight;
+            this.color = color;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        @SuppressWarnings("boxing") //컴파일시 컴파일 경고를 사용하지 않는다.
+        @Override
+        public String toString() {
+            return String.format("Apple{weight=%d, color='%s'}", weight, color);
+        }
+    }
+
+}
