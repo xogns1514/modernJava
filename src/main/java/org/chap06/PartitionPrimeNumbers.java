437a798d3559fbcd6b3892fd551bda8b9417452a
taehoon <xogns1514@gmail.com>
2023-09-06 11:37:57 +0900

    feat: chap6. CustomPrimeNumbersCollector

--- src/main/java/org/chap06/PartitionPrimeNumbers.java
+++ src/main/java/org/chap06/PartitionPrimeNumbers.java
@@ -1,13 +1,20 @@
 package org.chap06;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
 import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.partitioningBy;
 
 public class PartitionPrimeNumbers {
     public static void main(String[] args) {
+
+        Map<Boolean, List<Integer>> booleanListMap = partitionPrimesWithCustomCollector(50);
+        System.out.println(booleanListMap);
 
     }
 
@@ -22,10 +29,61 @@ public class PartitionPrimeNumbers {
         return primes.stream().noneMatch(i -> candidate % i == 0);
     }
 
+    public static boolean isPrime3(List<Integer> primes, int candidate) {
+        int candidateRoot = (int) Math.sqrt((double) candidate);
+        return primes.stream()
+                .takeWhile(i -> i <= candidateRoot)
+                .noneMatch(i -> candidate % i == 0); //소수로 나누어 떨어지는 것은 버림
+    }
     public static Map<Boolean, List<Integer>> partitionPrimes(int n) {
         return IntStream.rangeClosed(2, n).boxed()
                 .collect(partitioningBy(candidate -> isPrime(candidate)));
     }
 
+    public static Map<Boolean, List<Integer>> partitionPrimesWithCustomCollector(int n) {
+        return IntStream.rangeClosed(2, n).boxed()
+                .collect((new PrimeNumbersCollector()));
+    }
+
+    public static class PrimeNumbersCollector
+            implements Collector<Integer, Map<Boolean, List<Integer>>, Map<Boolean, List<Integer>>> {
+
+        @Override
+        public Supplier<Map<Boolean, List<Integer>>> supplier() {
+            return () -> new HashMap<Boolean, List<Integer>>(){
+                {
+                    put(true, new ArrayList<Integer>());
+                    put(false, new ArrayList<Integer>());
+                }
+            };
+        }
+
+        @Override
+        public BiConsumer<Map<Boolean, List<Integer>>, Integer> accumulator() {
+            return (Map<Boolean, List<Integer>> acc, Integer candidate) -> {
+                acc.get(isPrime3(acc.get(true), candidate))
+                        .add(candidate);
+            };
+        }
+
+        @Override
+        public BinaryOperator<Map<Boolean, List<Integer>>> combiner() {
+            return (Map<Boolean, List<Integer>> map1, Map<Boolean, List<Integer>> map2) -> {
+                map1.get(true).addAll(map2.get(true));
+                map1.get(false).addAll(map2.get(false));
+                return map1;
+            };
+        }
+
+        @Override
+        public Function<Map<Boolean, List<Integer>>, Map<Boolean, List<Integer>>> finisher() {
+            return Function.identity();
+        }
+
+        @Override
+        public Set<Characteristics> characteristics() {
+            return Collections.unmodifiableSet(EnumSet.of(Characteristics.IDENTITY_FINISH));
+        }
+    }
 
 }
